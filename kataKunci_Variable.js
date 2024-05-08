c126b2d4dacc46a3cca288ad13117be092774440
AyoonAJ <139105405+AyoonAJ@users.noreply.github.com>
2024-05-08 11:25:49 +0800

    upload file code javascript

---
+++ kataKunci_Variable.js
@@ -0,0 +1,18 @@
+// let untuk variable yang bssa diuba ubah
+// const untuk variable yang tidak bisa diubah ubah
+
+// belajar deklarasi variable
+let firstName = "Agus";
+let middleName = "Ayun";
+let lastName = "Ardi";
+const nama = "Agus";
+nama = "ardi"; // ini akan error karena data vab. nama tidak bisa diubah karena kuncinya const
+
+// buat menampilakn hasil output
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(nama); // in juga sudah pasti akan error
+
+// console.log(fisrtName, middleName, lastName)
+
